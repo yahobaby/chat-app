dd6309b65b8ef23f682445947a03aa1b14c73eb6
yahobaby <hellotechcamp@gmail.com>
2022-08-27 16:00:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  # 「name」カラムに、presence: trueを設けることで、空の場合はDBに保存しないというバリデーションを設定
+  # つまり、ユーザー登録時に「name」を空欄にして登録しようとすると、エラーが発生する。
 end
