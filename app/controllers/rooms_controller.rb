d9e544bac16bad5d6721e511b60d3bad20c54996
yahobaby <hellotechcamp@gmail.com>
2022-08-28 01:01:48 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,4 +5,25 @@ class RoomsController < ApplicationController
     # チャットルームの新規作成なので「new」アクションを定義
     # form_withに渡す引数として、値が空のRoomインスタンスを@roomに代入
   end
+
+  def create
+    # binding.pry
+    # paramsの値を確認する為、記述してたが、確認終わったので、削除
+    @room = Room.new(room_params)
+    # createアクション内の、保存の成功・失敗による条件分岐
+    # 保存が成功したかどうかで処理を分岐し、成功した場合はredirect_toメソッドでルートパスにリダイレクトし、失敗した場合はrenderメソッドでrooms/new.html.erbのページを表示
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  # Railsではセキュリティ対策として、保存する前にストロングパラメーターを使い、許可するパラメーターを指定してから、データを保存するよう推奨されている。いる。
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+    # user_ids: []記述 : 配列に対して保存を許可したい場合は、キーに対し[]を値として記述
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,6 @@ class Room < ApplicationRecord
   has_many :users, through: :room_users
     # 1つのチャットルームには、2人のユーザーが存在する。
 # また、1人のユーザーは複数のチャットルームに参加出来る。
+  validates :name, presence: true
+  # チャットルームを新規作成するにあたって「ルーム名」は必ず必要＆バリデーションは「ルーム名が存在（presence）している場合のみ作成可（true）」という意味
 end
