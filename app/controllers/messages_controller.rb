a13e19ffc0dc09ebca20dd6c540efb3cd2b32189
yahobaby <hellotechcamp@gmail.com>
2022-08-28 19:41:51 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,16 @@ class MessagesController < ApplicationController
     # @roomには、Room.find(params[:room_id])と記述することで、paramsに含まれているroom_idを代入
     # ルーティングをネストしているため/rooms/:room_id/messagesといったパスになる。
     # パスにroom_idが含まれているため、paramsというハッシュオブジェクトの中に、room_idという値が存在する。そのため、params[:room_id]と記述することでroom_idを取得できまる。
+
+
+    @messages = @room.messages.includes(:user)
+    # チャットルームに紐付いている全てのメッセージ（@room.messages）を@messagesと定義
+    # 一覧画面で表示するメッセージ情報には、ユーザー情報も紐付いて表示
+    # この場合、メッセージに紐付くユーザー情報の取得に、メッセージの数と同じ回数のアクセスが必要になるので、N+1問題が発生&その場合は、includesメソッドを使用して、解消する。
+    # 全てのメッセージ情報に紐づくユーザー情報を、includes(:user)と記述をすることにより、ユーザー情報を1度のアクセスでまとめて取得
+
+
+
   end
 
   def create
@@ -20,6 +30,11 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
+      # 投稿に失敗したときの処理にも、@messagesを定義
+      # renderを用いることで、投稿に失敗した@messageの情報を保持しつつindex.html.erbを参照できる。（この時、indexアクションは経由しない）
+      # しかしながら、そのときに@messagesが定義されていないとエラーになってしまうので、indexアクションと同様に@messagesを定義する必要があり。
+
       render :index
       # メッセージの保存に失敗した場合、renderメソッドを用いてindexアクションのindex.html.erbを表示するように指定
       # このとき、createアクションのインスタンス変数はそのままindex.html.erbに渡され、同じページに戻る。
